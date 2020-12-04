74de76bdab8ea8fb03bc53425365e3db25200f24
Saif Ali Shaik <saifshine7@gmail.com>
2020-12-04 22:36:44 +0530

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const clearConsole = require('clear-any-console');
+
+// Clears the console
+clearConsole();
+
+console.log(`Hello Saif! - Developer Advocate - Hello!
+
+Half-human, half-Atlantean Arthur is born with the ability to
+communicate with marine creatures. He goes on a quest to retrieve
+the legendary Trident of Atlan and protect the water world.
+
+📧 aquaman@ocean.master
+☏ 7854-996-1245
+`);
