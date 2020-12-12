27d93ceb8afc8ea47b470c1c72fb88b113858fc3
Saif Ali Shaik <saifshine7@gmail.com>
2020-12-12 21:00:12 +0530

    📦 NEW: check node version

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
 const chalk = require('chalk');
+const alert = require('cli-alerts-saif');
 const log = console.log;
+const checkNode = require('cli-check-node');
+
 const dim = chalk.dim;
 const italic = chalk.italic;
 const twitterClr = chalk.hex(`1da1f2`).bold.inverse;
 const githubClr = chalk.hex(`6cc644`).bold.inverse;
 const purple = chalk.hex(`6937ff`).bold.inverse;
+
+// Alerts
+const sym = require('log-symbols');
+const success = chalk.green;
+const warning = chalk.keyword(`orange`);
+const error = chalk.red.bold;
+const info = chalk.blue;
 
 welcome({
   title: `Saif Shines`,
@@ -17,16 +27,27 @@ welcome({
   bgColor: `#FADC00`,
   color: `#000000`,
   bold: true,
-  clear: true
+  clear: false
 });
 
 log(`
-${italic(`Half-human, half-Atlantean Arthur is born with the ability to
-communicate with marine creatures. He goes on a quest to retrieve
-the legendary Trident of Atlan and protect the water world.`)}
+${italic(
+  `Half-human, half-Atlantean Arthur is born with the ability to communicate with marine creatures. He goes on a quest to retrieve the legendary Trident of Atlan and protect the water world.`
+)}
 
 ${twitterClr(` Twitter `)}: ${dim(`https://twitter.com/Saif_Shines`)}
 ${githubClr(` Github `)}: ${dim(`https://github.com/Saif-Shines`)}
 ${purple(` Blog `)}: ${dim(`https://www.linkedin.com/today/author/saif-shines`)}
 
 `);
+
+log(`
+${sym.success} ${success(` SUCCESS `)}: Thank of checking out my CLI
+${sym.info} ${info(` INFO `)}: I'm leaning how to improve fdk
+${sym.warning} ${warning(` WARNING `)}: Please don't copy
+${sym.error} ${error(` ERROR `)}: I'm on vacation. Contact me next week.
+`);
+
+checkNode('10');
+
+//alert({ type: 'success', msg: 'randome message', name: 'wow' });
